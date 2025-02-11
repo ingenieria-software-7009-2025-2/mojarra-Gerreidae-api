572f60c83f950266a83112370302fe4ae82f3988
Juvenal Guzmán Condado <juvsguzman@gmail.com>
2025-02-11 16:48:45 -0600

    Primer commit

---
+++ src/main/kotlin/com/mojarras_team/mojarra_Gerreidae_api/MojarraGerreidaeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mojarras_team.mojarra_Gerreidae_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MojarraGerreidaeApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MojarraGerreidaeApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mojarras_team/mojarra_Gerreidae_api/MojarraGerreidaeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mojarras_team.mojarra_Gerreidae_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MojarraGerreidaeApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
