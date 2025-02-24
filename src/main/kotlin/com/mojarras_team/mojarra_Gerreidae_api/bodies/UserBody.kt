d0f2542b08489837dfbe73eecaf81149122c7dff
Juvenal Guzmán Condado <juvsguzman@gmail.com>
2025-02-24 15:32:27 -0600

    Agrega clases para permitir el login

---
+++ src/main/kotlin/com/mojarras_team/mojarra_Gerreidae_api/bodies/UserBody.kt
@@ -0,0 +1,7 @@
+package com.mojarras_team.mojarra_Gerreidae_api.bodies
+
+data class UserBody(
+    val mail : String = "",
+    val password : String = "",
+    val token : String = ""
+)
---
+++ src/main/kotlin/com/mojarras_team/mojarra_Gerreidae_api/bodies/UserLogInBody.kt
@@ -0,0 +1,6 @@
+package com.mojarras_team.mojarra_Gerreidae_api.bodies
+
+data class UserLogInBody(
+    val mail : String = "",
+    val password : String = ""
+)
