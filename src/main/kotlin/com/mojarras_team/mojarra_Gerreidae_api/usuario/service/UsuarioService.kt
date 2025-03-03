6f8e22f198b3ea3bfc190a5b11cf59b87a3a2c12
Juvenal <38957852+JuvsProgramer@users.noreply.github.com>
2025-03-03 15:20:38 -0600

    Crea userService

---
+++ src/main/kotlin/com/mojarras_team/mojarra_Gerreidae_api/usuario/service/UsuarioService.kt
@@ -0,0 +1,10 @@
+package com.mojarras_team.mojarra_Gerreidae_api.usuario.service
+
+import com.mojarras_team.mojarra_Gerreidae_api.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UsuarioService (private var usuarioRepo : UserRepository) {
+
+    fun crearUsuario(usuario)
+}
